10fe0e1f41bec8a16b9d68898dc6c99c31452bee
dkosolobov <dkosolobov@mail.ru>
2021-03-20 20:38:06 +0500

    +C# implementation +tests comparing vs SortedSet

---
+++ BtreeSimple.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BtreeCs
+{
+	class Bnode<Tkey> where Tkey : IComparable<Tkey>
+	{
+		const int b = 16;   // sizeof(key) * b = ~64 (длина кеш линии)
+		public byte Count = 0;
+		public Tkey[] Keys = new Tkey[2 * b + 1];  // последний key не используется для упрощения реализации
+		public Bnode<Tkey>[] Kids = null;
+		public Bnode(bool leaf)
+		{   // последний kid не используется для упрощения реализации
+			Kids = leaf? null : new Bnode<Tkey>[2 * b + 2]; 
+		}
+		public bool Search(Tkey key)
+		{
+			int i = 0;  // Array.FindIndex и BinarySearch заметно медленнее
+			while (i < Count && Keys[i].CompareTo(key) < 0)
+				i++;    // на коротких массивах линейный поиск быстрее бинарного
+			if (i < Count && Keys[i].Equals(key))
+				return true;
+			return Kids != null && Kids[i].Search(key);
+		}
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public (Tkey, Bnode<Tkey>) InsertAtPos(int pos, Tkey key, Bnode<Tkey> nodeAfterKey)
+		{
+			Array.Copy(Keys, pos, Keys, pos + 1, Count - pos);
+			Keys[pos] = key;  // "раздвигаем" Keys и kids, чтобы вставить key и node_after_key
+			if (Kids != null)
+			{
+				Array.Copy(Kids, pos + 1, Kids, pos + 2, Count - pos);
+				Kids[pos + 1] = nodeAfterKey;
+			}
+			if (++Count <= 2 * b)
+				return (default(Tkey), (Bnode<Tkey>)null);  // "наверх" ничего не идёт
+			var split = new Bnode<Tkey>(Kids == null);
+			Count = split.Count = b;  // в node сейчас 2b+1 ключей; node.Keys[b] "пойдёт наверх"
+			Array.Copy(Keys, b + 1, split.Keys, 0, b);
+			if (Kids != null)
+				Array.Copy(Kids, b + 1, split.Kids, 0, b + 1);
+			return (Keys[b], split);  // ключ node.Keys[b] не входит в node и "идёт наверх"
+		}
+		public (Tkey, Bnode<Tkey>) Insert(Tkey key)
+		{
+			int pos = 0;  // Array.FindIndex и BinarySearch заметно медленнее
+			while (pos < Count && Keys[pos].CompareTo(key) < 0)
+				pos++;    // на коротких массивах линейный поиск быстрее бинарного
+			Bnode<Tkey> overflow_node = null;
+			if (Kids != null)
+				(key, overflow_node) = Kids[pos].Insert(key);
+			if (Kids == null || overflow_node != null)
+				return InsertAtPos(pos, key, overflow_node);
+			return (default(Tkey), null);
+		}
+	}
+	class BtreeSimple<Tkey> where Tkey : IComparable<Tkey>
+	{
+		private Bnode<Tkey> root = new Bnode<Tkey>(leaf: true);
+		public bool Search(Tkey key) { return root.Search(key); }
+		public void Insert(Tkey key)
+		{
+			(Tkey okey, Bnode<Tkey> overflow_node) = root.Insert(key);
+			if (overflow_node != null)
+			{   // увеличиваем высоту дерева "вверх"
+				var old_root = root;
+				root = new Bnode<Tkey>(false) { Count = 1 };
+				(root.Keys[0], root.Kids[0], root.Kids[1]) = (okey, old_root, overflow_node);
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BtreeCs
+{
+    class Program
+    {
+        const int n = 10000000; //n queries: 3/4 of them insert, 1/4 search
+        static void GenerateRandomTests(List<int> content, List<int> seq, List<int> op)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                int choice = rnd.Next(0, int.MaxValue) % 4;
+                if (choice < 3 || content.Count == 0)
+                {
+                    int val = rnd.Next(0, int.MaxValue);
+                    seq.Add(val);
+                    op.Add(0);
+                    content.Add(val);
+                }
+                else
+                {
+                    int val;
+                    if (rnd.Next(0, int.MaxValue) % 5 != 1)
+                        val = content[rnd.Next(0, int.MaxValue) % content.Count];
+                    else
+                        val = rnd.Next(0, int.MaxValue);
+                    seq.Add(val);
+                    op.Add(1);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            var content = new List<int>();
+            var seq = new List<int>();
+            var op = new List<int>();
+            GenerateRandomTests(content, seq, op);
+
+            Console.WriteLine("Start!");
+
+            int sum1 = 0;
+            var btree = new BtreeSimple<int>(); 
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < n; i++)
+            {
+                if (op[i] == 0)
+                    btree.Insert(seq[i]);
+                else if (op[i] == 1)
+                    sum1 += btree.Search(seq[i]) ? 1 : 0;
+            }
+            watch.Stop();
+            Console.WriteLine($"duration B-tree = {watch.ElapsedMilliseconds} ms");
+
+            int sum2 = 0;
+            var set = new SortedSet<int>();
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < n; i++)
+            {
+                if (op[i] == 0)
+                    set.Add(seq[i]);
+                else if (op[i] == 1)
+                    sum2 += set.Contains(seq[i]) ? 1 : 0;
+            }
+            watch.Stop();
+            Console.WriteLine($"duration set    = {watch.ElapsedMilliseconds} ms");
+
+            if (sum1 != sum2)
+                Console.WriteLine($"Sums are different: {sum1} {sum2}");
+        }
+    }
+}
