b064fd9f48fc7f494b004c5274dca74b3d0b5f3b
dkosolobov <dkosolobov@mail.ru>
2021-03-21 20:57:58 +0500

    Refactored C++ version adding split function

--- btree_simple.hpp
+++ btree_simple.hpp
@@ -6,11 +6,11 @@ static const int b = 16;  // sizeof(key) * b = 64 (длина кеш линии)
 static struct bleaf {
 	bool leaf;
 	uint8_t count;
-	uint32_t keys[2 * b + 1];  // keys[2*b] не используется (он нужен для упрощения реализации)
+	uint32_t keys[2 * b + 1];  // keys[2*b] не используется (но нужен для упрощения реализации)
 };
 
-static struct bnode : bleaf {
-	bnode* kids[2 * b + 2];    // kids[2*b+1] не используется (он нужен для упрощения реализации)
+static struct bnode : public bleaf {
+	bnode* kids[2 * b + 2];    // kids[2*b+1] не используется (но нужен для упрощения реализации)
 
 	bool search(uint32_t key) const {
 		size_t i = 0;
@@ -19,23 +19,6 @@ static struct bnode : bleaf {
 		if (i < count && keys[i] == key)
 			return true;
 		return !leaf && kids[i]->search(key);
-	}
-
-	std::pair<uint32_t, bnode*> insert_at_pos(size_t pos, uint32_t key, bnode* node_after_key) {
-		memmove(keys + pos + 1, keys + pos, (count - pos) * sizeof(keys[0]));
-		keys[pos] = key;  // "раздвигаем" keys и kids, чтобы вставить key и node_after_key
-		if (!leaf) {
-			memmove(kids + pos + 2, kids + pos + 1, (count - pos) * sizeof(kids[0]));
-			kids[pos + 1] = node_after_key;
-		}
-		if (++count <= 2 * b)
-			return { 0, nullptr };
-		auto split = leaf ? (bnode*) new bleaf{ true } : new bnode{ false };
-		count = split->count = b;  // в keys сейчас 2b+1 ключей; keys[b] пойдёт "наверх"
-		std::copy(keys + b + 1, keys + 2 * b + 1, split->keys);
-		if (!leaf)
-			std::copy(kids + b + 1, kids + 2 * b + 2, split->kids);
-		return { keys[b], split };  // ключ keys[b] не входит в узел и идёт "наверх"
 	}
 
 	std::pair<uint32_t, bnode*> insert(uint32_t key) {
@@ -48,6 +31,28 @@ static struct bnode : bleaf {
 		if (leaf || overflow_node != nullptr)
 			return insert_at_pos(pos, key, overflow_node);
 		return { 0, nullptr };
+	}
+
+private:
+	std::pair<uint32_t, bnode*> insert_at_pos(size_t pos, uint32_t key, bnode* node_after_key) {
+		memmove(keys + pos + 1, keys + pos, (count - pos) * sizeof(keys[0]));
+		keys[pos] = key;  // "раздвигаем" keys и kids, чтобы вставить key и node_after_key
+		if (!leaf) {
+			memmove(kids + pos + 2, kids + pos + 1, (count - pos) * sizeof(kids[0]));
+			kids[pos + 1] = node_after_key;
+		}
+		if (++count <= 2 * b)
+			return { 0, nullptr };
+		return split();
+	}
+
+	std::pair<uint32_t, bnode*> split() {  // в keys 2b+1 ключей, keys[b] - медиана
+		auto split = leaf ? (bnode*) new bleaf{ true } : new bnode{ false };
+		count = split->count = b;
+		std::copy(keys + b + 1, keys + 2 * b + 1, split->keys);
+		if (!leaf)
+			std::copy(kids + b + 1, kids + 2 * b + 2, split->kids);
+		return { keys[b], split };  // keys[b] поднимается на уровень выше
 	}
 };
 
