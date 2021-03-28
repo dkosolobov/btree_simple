aae2a50b6fec587ad7baf9ff5a2b14a5448dd881
dkosolobov <dkosolobov@mail.ru>
2021-03-28 19:15:45 +0500

    Refactoring: shortened names, renamed search->find

--- btree_simple.hpp
+++ btree_simple.hpp
@@ -2,52 +2,52 @@
 #include <algorithm>
 #include <tuple>
 
-static struct bleaf {
-	static const int b = 16;  // sizeof(key) * b = 64 (длина кеш линии)
+static const int b = 16;  // sizeof(key) * b = ~64 (длина кеш линии)
+struct bleaf {
 	bool leaf;
 	uint8_t count;
-	uint32_t keys[2 * b + 1];  // keys[2*b] не используется (но нужен для упрощения реализации)
+	int keys[2 * b + 1];  // keys[2*b] не используется (но нужен для упрощения реализации)
 };
 
-static struct bnode : public bleaf {
+struct bnode : bleaf {
 	bnode* kids[2 * b + 2];    // kids[2*b+1] не используется (но нужен для упрощения реализации)
 
-	bool search(uint32_t key) const {
+	bool find(int key) const {
 		size_t i = 0;
 		while (i < count && keys[i] < key)
 			i++;   // на коротких массивах линейный поиск быстрее бинарного
 		if (i < count && keys[i] == key)
 			return true;
-		return !leaf && kids[i]->search(key);
+		return !leaf && kids[i]->find(key);
 	}
 
-	std::pair<uint32_t, bnode*> insert(uint32_t key) {
-		size_t pos = 0;
-		while (pos < count && keys[pos] < key)
-			pos++;  // на коротких массивах линейный поиск быстрее бинарного
-		bnode* overflow_node = nullptr;
+	std::pair<int, bnode*> insert(int key) {
+		size_t i = 0;
+		while (i < count && keys[i] < key)
+			i++;  // на коротких массивах линейный поиск быстрее бинарного
+		bnode* overflow = nullptr;
 		if (!leaf)
-			std::tie(key, overflow_node) = kids[pos]->insert(key);
-		if (leaf || overflow_node != nullptr)
-			return insert_at_pos(pos, key, overflow_node);
-		return { 0, nullptr };
+			std::tie(key, overflow) = kids[i]->insert(key);
+		if (!leaf && overflow == nullptr)
+			return { 0, nullptr };
+		return insert_at(i, key, overflow);
 	}
 
 private:
-	std::pair<uint32_t, bnode*> insert_at_pos(size_t pos, uint32_t key, bnode* node_after_key) {
-		memmove(keys + pos + 1, keys + pos, (count - pos) * sizeof *keys);
-		keys[pos] = key;  // "раздвигаем" keys и kids, чтобы вставить key и node_after_key
+	std::pair<int, bnode*> insert_at(size_t i, int key, bnode* node_after_key) {
+		memmove(keys + i + 1, keys + i, (count - i) * sizeof *keys);
+		keys[i] = key;  // раздвигаем keys и kids, чтобы вставить key и node_after_key
 		if (!leaf) {
-			memmove(kids + pos + 2, kids + pos + 1, (count - pos) * sizeof *kids);
-			kids[pos + 1] = node_after_key;
+			memmove(kids + i + 2, kids + i + 1, (count - i) * sizeof *kids);
+			kids[i + 1] = node_after_key;
 		}
 		if (++count <= 2 * b)
 			return { 0, nullptr };
 		return split();
 	}
 
-	std::pair<uint32_t, bnode*> split() {  // в keys 2b+1 ключей, keys[b] - медиана
-		auto split = leaf ? (bnode*) new bleaf{ true } : new bnode{ false };
+	std::pair<int, bnode*> split() {  // в keys 2b+1 ключей, keys[b] - медиана
+		auto split = leaf ? (bnode*) new bleaf{true} : new bnode{false};
 		count = split->count = b;
 		std::copy(keys + b + 1, keys + 2 * b + 1, split->keys);
 		if (!leaf)
@@ -57,11 +57,11 @@ private:
 };
 
 class btree_simple {
-	bnode* root = (bnode*) new bleaf{ true };
+	bnode* root = (bnode*) new bleaf{true};
 public:
-	bool search(uint32_t key) const { return root->search(key); }
+	bool find(int key) const { return root->find(key); }
 
-	void insert(uint32_t key) {
+	void insert(int key) {
 		auto [okey, overflow] = root->insert(key);
 		if (overflow != nullptr)  // увеличиваем высоту дерева "вверх"
 			root = new bnode{ false, 1, { okey }, { root, overflow } };
