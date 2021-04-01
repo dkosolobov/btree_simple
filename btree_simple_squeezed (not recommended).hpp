6da54d4dcd2bf94770d3f09ec500c7def0fb39fd
dkosolobov <dkosolobov@mail.ru>
2021-04-01 22:21:52 +0500

    Added squeezed ICPC-style version (bad style)

---
+++ btree_simple_squeezed (not recommended).hpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <tuple>
+using namespace std;
+
+const int b = 16; // sizeof(key) * b = 64 (длина кеш линии)
+struct leaf {
+	bool l;
+	uint8_t n;
+	int ks[2 * b + 1];
+};
+
+struct node : leaf {
+	node* kids[2 * b + 2];
+
+	bool find(int k) const {
+		int i = 0;
+		while (i < n && ks[i] < k) i++;
+		return (i < n && ks[i] == k) || (!l && kids[i]->find(k));
+	}
+
+	pair<int, node*> insert(int k) {
+		int i = 0;
+		while (i < n && ks[i] < k) i++;
+		if (l) return insert_at(i, k, 0);
+		auto [p, x] = kids[i]->insert(k);
+		if (!x) return { 0, 0 };
+		return insert_at(i, p, x);
+	}
+	
+private:
+	pair<int, node*> insert_at(int i, int k, node* x) {
+		memmove(ks + i + 1, ks + i, (n - i) * sizeof *ks);
+		ks[i] = k;
+		if (!l) {
+			memmove(kids + i + 2, kids + i + 1, (n - i) * sizeof *kids);
+			kids[i + 1] = x;
+		}
+		if (++n <= 2 * b) return { 0, 0 };
+		return split();
+	}
+
+	pair<int, node*> split() {
+		auto x = l ? (node*) new leaf{true} : new node{false};
+		n = x->n = b;
+		memmove(x->ks, ks + b + 1, b * sizeof *ks);
+		if (!l) memmove(x->kids, kids + b + 1, (b + 1) * sizeof *kids);
+		return { ks[b], x };
+	}
+};
+
+node* root = (node*) new leaf{true};
+
+void insert(int k) {
+	auto [p, x] = root->insert(k);
+	if (x) root = new node{ false, 1, { p }, { root, x } };
+}
